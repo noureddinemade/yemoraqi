7614cecd16187aa6ce4f7c606ad9d153e53c7fa1
Noureddine Azhar <nrdnazhr@gmail.com>
2020-03-20 15:53:47 +1100

    Added new styles, updated fonts

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,26 @@
 //
 
+const bgSpiral      = document.querySelector('div.bg');
+const mainText      = document.querySelector('section h1');
+
 //
+
+const moveObject    = (obj, x, y) => {
+
+    obj.style.transform = `translate(${x}px,${y}px)`
+
+}
+
+//
+
+document.addEventListener('mousemove', (e) => {
+
+    let x = e.pageX;
+    let y = e.pageY;
+
+    let spiralX = x / 10;
+    let spiralY = y / 10;
+
+    moveObject(bgSpiral, spiralX, spiralY);
+
+});
